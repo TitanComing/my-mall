aa1f7ba096ffbb8e53f6d8d08dc54c93d3618b3d
peng <1740298094@qq.com>
2020-05-14 22:44:08 +0800

    日常提交

--- mall-admin/src/main/java/com/peng/mall/service/impl/PmsProductServiceImpl.java
+++ mall-admin/src/main/java/com/peng/mall/service/impl/PmsProductServiceImpl.java
@@ -1,12 +1,21 @@
 package com.peng.mall.service.impl;
 
-import com.peng.mall.dao.PmsMemberPriceDao;
-import com.peng.mall.mapper.PmsProductMapper;
+import cn.hutool.core.collection.CollectionUtil;
+import com.peng.mall.dao.*;
+import com.peng.mall.dto.PmsProductParam;
+import com.peng.mall.mapper.*;
+import com.peng.mall.model.PmsProduct;
 import com.peng.mall.service.PmsProductService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.lang.reflect.Member;
+import java.lang.reflect.Method;
+import java.util.Collection;
+import java.util.List;
 
 /**
  * 商品管理Service实现类
@@ -20,5 +29,74 @@ public class PmsProductServiceImpl implements PmsProductService {
     private PmsProductMapper productMapper;
     @Autowired
     private PmsMemberPriceDao memberPriceDao;
+    @Autowired
+    private PmsMemberPriceMapper memberPriceMapper;
+    @Autowired
+    private PmsProductLadderDao productLadderDao;
+    @Autowired
+    private PmsProductLadderMapper productLadderMapper;
+    @Autowired
+    private PmsProductFullReductionDao productFullReductionDao;
+    @Autowired
+    private PmsProductFullReductionMapper productFullReductionMapper;
+    @Autowired
+    private PmsSkuStockDao skuStockDao;
+    @Autowired
+    private PmsSkuStockMapper skuStockMapper;
+    @Autowired
+    private PmsProductAttributeValueDao productAttributeValueDao;
+    @Autowired
+    private PmsProductAttributeValueMapper productAttributeValueMapper;
+    @Autowired
+    private CmsSubjectProductRelationDao subjectProductRelationDao;
+    @Autowired
+    private CmsSubjectProductRelationMapper subjectProductRelationMapper;
+    @Autowired
+    private CmsPrefrenceAreaProductRelationDao prefrenceAreaProductRelationDao;
+    @Autowired
+    private CmsPrefrenceAreaProductRelationMapper prefrenceAreaProductRelationMapper;
+    @Autowired
+    private PmsProductDao productDao;
+    @Autowired
+    private PmsProductVertifyRecordDao productVertifyRecordDao;
+
+    @Override
+    public int create(PmsProductParam productParam) {
+        int count;
+        // 创建商品
+        PmsProduct product = productParam;
+        product.setId(null);
+        productMapper.insertSelective(product);
+        //根据促销类型设置价格：会员价格、阶梯价格、满减价格
+        Long productId = product.getId();
+
+
+        return 0;
+    }
+
+    /**
+     * 建立和插入关系表操作
+     *
+     * @param dao       可以操作的dao
+     * @param dataList  要插入的数据
+     * @param productId 建立关系的id
+     */
+    private void relateAndInsertList(Object dao, List dataList, Long productId) {
+        try {
+            if (CollectionUtils.isEmpty(dataList)) return;
+            for (Object item : dataList) {
+                Method setId = item.getClass().getMethod("setId", Long.class);
+                setId.invoke(item, (Long) null);
+                Method setProductId = item.getClass().getMethod("setProductId", Long.class);
+                setProductId.invoke(item, productId);
+            }
+            Method insertList = dao.getClass().getMethod("insertList", List.class);
+            insertList.invoke(dao, dataList);
+        } catch (Exception e) {
+            LOGGER.warn("创建产品出错:{}", e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+    }
+
 
 }
