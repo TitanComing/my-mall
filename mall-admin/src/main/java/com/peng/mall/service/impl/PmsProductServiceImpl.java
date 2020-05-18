5836ea6da378a77c9967c24c0c55de8a5099d581
peng <1740298094@qq.com>
2020-05-18 23:13:37 +0800

    日常提交20200518

--- mall-admin/src/main/java/com/peng/mall/service/impl/PmsProductServiceImpl.java
+++ mall-admin/src/main/java/com/peng/mall/service/impl/PmsProductServiceImpl.java
@@ -3,9 +3,12 @@ package com.peng.mall.service.impl;
 import cn.hutool.core.collection.CollectionUtil;
 import com.peng.mall.dao.*;
 import com.peng.mall.dto.PmsProductParam;
+import com.peng.mall.dto.PmsProductResult;
 import com.peng.mall.mapper.*;
 import com.peng.mall.model.PmsProduct;
+import com.peng.mall.model.PmsSkuStock;
 import com.peng.mall.service.PmsProductService;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +17,9 @@ import org.springframework.util.CollectionUtils;
 
 import java.lang.reflect.Member;
 import java.lang.reflect.Method;
+import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -63,13 +68,62 @@ public class PmsProductServiceImpl implements PmsProductService {
     @Override
     public int create(PmsProductParam productParam) {
         int count;
-        // 创建商品
+        // 创建商品-将子类实例赋值给父类，实际等于抹除子类添加的多余信息，相当于对productParam进行了分解
         PmsProduct product = productParam;
         product.setId(null);
         productMapper.insertSelective(product);
         //根据促销类型设置价格：会员价格、阶梯价格、满减价格
         Long productId = product.getId();
+        //会员价格
+        relateAndInsertList(memberPriceDao,productParam.getMemberPriceList(),productId);
+        //阶梯价格
+        relateAndInsertList(productLadderDao,productParam.getProductLadderList(),productId);
+        //满减价格
+        relateAndInsertList(productFullReductionDao,productParam.getProductFullReductionList(),productId);
+        //处理sku的编码
+        handleSkuStockCode(productParam.getSkuStockList(), productId);
+        //添加sku库存信息
+        relateAndInsertList(skuStockDao, productParam.getSkuStockList(), productId);
+        //添加商品参数，添加自定义商品规格
+        relateAndInsertList(productAttributeValueDao, productParam.getProductAttributeValueList(), productId);
+        //关联专题
+        relateAndInsertList(subjectProductRelationDao, productParam.getSubjectProductRelationList(), productId);
+        //关联优选
+        relateAndInsertList(prefrenceAreaProductRelationDao, productParam.getPrefrenceAreaProductRelationList(), productId);
 
+        count = 1;
+        return count;
+    }
+
+    private void handleSkuStockCode(List<PmsSkuStock> skuStockList, Long productId) {
+        if(CollectionUtil.isEmpty(skuStockList)) return;
+        for(int i=0; i < skuStockList.size(); i++){
+            PmsSkuStock skuStock = skuStockList.get(i);
+            if(StringUtils.isEmpty(skuStock.getSkuCode())){
+                // 商品构造器，编码方式的写法
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+                StringBuilder sb = new StringBuilder();
+                //日期
+                sb.append(sdf.format(new Date()));
+                //四位商品id
+                sb.append(String.format("%04d", productId));
+                //三位索引id
+                sb.append(String.format("%03d", i+1));
+                skuStock.setSkuCode(sb.toString());
+            }
+        }
+    }
+
+    @Override
+    public PmsProductResult getUpdateInfo(Long id) {
+        return productDao.getUpdateInfo(id);
+    }
+
+    @Override
+    public int update(Long id, PmsProductParam productParam) {
+        int count;
+        //更新商品信息
+        PmsProduct product = productParam;
 
         return 0;
     }
